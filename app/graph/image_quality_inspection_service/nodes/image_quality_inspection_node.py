509df28a679f5cd0175fff9b2c25a11b85b5a8b0
dbs727 <dhjsjs0123@gmail.com>
2026-08-06 10:21:05 +0900

    fix: vlm 자체 성능 테스트

--- app/graph/image_quality_inspection_service/nodes/image_quality_inspection_node.py
+++ app/graph/image_quality_inspection_service/nodes/image_quality_inspection_node.py
@@ -7,48 +7,54 @@ from app.graph.image_quality_inspection_service.state import QualityState
 async def image_quality_inspection_node(state: QualityState):
     images = state.get("images", [])
     image_type = state.get("imageType","")
-    
-    reference_cases = state.get("reference_cases", [])
-    ref_dict = {ref["target_imageId"]: ref for ref in reference_cases}
 
     all_image_urls = []
-    context_parts = []
-
     for img in images:
-        target_id = img["imageId"]
-        ref = ref_dict.get(target_id) 
-        context_parts.append(f"\n--- 분석 대상 ID: {target_id} ---")
+       all_image_urls.append(img["imageUrl"])
+    # reference_cases = state.get("reference_cases", [])
+    # ref_dict = {ref["target_imageId"]: ref for ref in reference_cases}
+
+    # all_image_urls = []
+    # context_parts = []
+
+    # for img in images:
+    #     target_id = img["imageId"]
+    #     ref = ref_dict.get(target_id) 
+    #     context_parts.append(f"\n--- 분석 대상 ID: {target_id} ---")
         
-        if ref:
-            all_image_urls.append(ref["ref_imageUrl"])
-            ref_idx = len(all_image_urls)
+    #     if ref:
+    #         all_image_urls.append(ref["ref_imageUrl"])
+    #         ref_idx = len(all_image_urls)
             
-            all_image_urls.append(img["imageUrl"])
-            target_idx = len(all_image_urls)
+    #         all_image_urls.append(img["imageUrl"])
+    #         target_idx = len(all_image_urls)
             
-            context_parts.append(
-                f"* 이미지 {ref_idx} (과거 불량 사례): 판정 유형 [{ref['ref_failType']}]\n"
-                f"* 이미지 {target_idx} (실제 분석 대상): 이미지 {ref_idx}(과거 사례)를 우선 참고하되, 두 이미지의 결함 양상이 명확히 다르다고 판단되면 과거 사례를 무시하고 [판별 기준]에 따라 독립적으로 판독하세요."
-            )
-        else:
-            all_image_urls.append(img["imageUrl"])
-            target_idx = len(all_image_urls)
-            context_parts.append(
-                f"* 이미지 {target_idx} (실제 분석 대상): 과거 참고 사례가 없습니다. 독립적으로 판독하세요."
-            )
-    context = "\n".join(context_parts)
+    #         context_parts.append(
+    #             f"* 이미지 {ref_idx} (과거 불량 사례): 판정 유형 [{ref['ref_failType']}]\n"
+    #             f"* 이미지 {target_idx} (실제 분석 대상): 이미지 {ref_idx}(과거 사례)를 우선 참고하되, 두 이미지의 결함 양상이 명확히 다르다고 판단되면 과거 사례를 무시하고 [판별 기준]에 따라 독립적으로 판독하세요."
+    #         )
+    #     else:
+    #         all_image_urls.append(img["imageUrl"])
+    #         target_idx = len(all_image_urls)
+    #         context_parts.append(
+    #             f"* 이미지 {target_idx} (실제 분석 대상): 과거 참고 사례가 없습니다. 독립적으로 판독하세요."
+    #         )
+    # context = "\n".join(context_parts)
+
+    # 당신에게 두 그룹(과거 불량 사례, 분석 대상 이미지)의 이미지가 번갈아서 제공됩니다.
+    
+    # [분석 세트 안내]
+    # {context}
+
+    # "ref_failtype": 참고한 이미지 실패 케이스
 
     if image_type == "CT":
       system_msg = "당신은 배터리 내부 구조 CT 검사 영상의 무결성을 판독하는 AI 품질 엔지니어입니다. 반드시 지정된 JSON 형식으로만 응답하세요."
       
       user_msg = f"""
       제공된 이미지는 배터리 셀의 내부 CT 촬영 영상입니다.
-      당신에게 두 그룹(과거 불량 사례, 분석 대상 이미지)의 이미지가 번갈아서 제공됩니다.
       분석 대상 이미지를 분석하여 이미지 당 하나의 어떤 물리적 환경 및 설정 오류로 인한 화질 불량 원인을 판별하세요.
       
-      [분석 세트 안내]
-      {context}
-
       [판별 기준: CT 촬영 실패 케이스]
       * ct_cell_alignment_failure: 배터리 일부가 촬영 영역 밖으로 벗어나 외곽과 내부 구조 일부가 잘림.
       * ct_acquisition_motion: 구조 경계의 방향성 흐림, 동일 구조가 이동 방향으로 이중으로 보이는 ghosting.
@@ -59,7 +65,7 @@ async def image_quality_inspection_node(state: QualityState):
 
       [출력 JSON 형식]
       [
-        {{"imageId":"분석한 이미지 ID", "ref_failtype": 참고한 이미지 실패 케이스, "failType":"판별 기준에 명시된 실패 케이스 ID", "description":"발견된 현상에 대한 시각적 근거 요약"}},
+        {{"imageId":"분석한 이미지 ID", "failType":"판별 기준에 명시된 실패 케이스 ID", "description":"발견된 현상에 대한 시각적 근거 요약"}},
         ...
       ]
       """
@@ -67,12 +73,8 @@ async def image_quality_inspection_node(state: QualityState):
       system_msg = "당신은 배터리 외관 표면 RGB 검사 영상의 무결성을 판독하는 AI 품질 엔지니어입니다. 반드시 지정된 JSON 형식으로만 응답하세요."
       
       user_msg = f"""
-      제공된 이미지는 배터리 셀의 외부 표면을 촬영한 RGB 영상입니다. 
-      당신에게 두 그룹(과거 불량 사례, 분석 대상 이미지)의 이미지가 번갈아서 제공됩니다.
+      제공된 이미지는 배터리 셀의 외부 표면을 촬영한 RGB 영상입니다.
       분석 대상 이미지를 분석하여 이미지 당 하나의 어떤 물리적 환경 및 설정 오류로 인한 화질 불량 원인을 판별하세요.
-
-      [분석 세트 안내]
-      {context}
 
       [판별 기준: RGB 촬영 실패 케이스]
       * rgb_trigger_timing_failure: 배터리 앞부분 또는 뒷부분이 잘린 이미지.
@@ -87,7 +89,7 @@ async def image_quality_inspection_node(state: QualityState):
 
       [출력 형식]
       [
-        {{"imageId":"분석한 이미지 ID", "ref_failtype": 참고한 이미지 실패 케이스, "failType":"판별 기준에 명시된 실패 케이스 ID", "description":"발견된 현상에 대한 시각적 근거 요약"}},
+        {{"imageId":"분석한 이미지 ID", "failType":"판별 기준에 명시된 실패 케이스 ID", "description":"발견된 현상에 대한 시각적 근거 요약"}},
         ...
       ]
       """
@@ -97,4 +99,4 @@ async def image_quality_inspection_node(state: QualityState):
 
     data_list = json.loads(clean_response)
 
-    return {"inspection_result": response_text}
+    return {"inspection_result": data_list}
